abea0f784cb9d5ae850551fb1dc327447ea262e0
morganric <morganric@gmail.com>
2014-09-11 20:01:06 +0100

    fix

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,12 @@
 class VisitorsController < ApplicationController
 
 def index
-	@clubs = Club.all.as_json
+	@clubs = Club.all
+	@hash = Gmaps4rails.build_markers(@clubs) do |club, marker|
+	  marker.lat club.latitude
+	  marker.lng club.longitude
+	  marker.infowindow "<a href='#{club_path(club.slug)}'>#{club.name}</a>"
+	end
 end
 
 end
