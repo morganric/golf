3c2d00d5145d8518559d3ca5414f351bdb64076b
morganric <morganric@gmail.com>
2014-09-18 15:56:48 +0100

    logo

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,14 +1,15 @@
 class VisitorsController < ApplicationController
 
+	include ActionView::Helpers::AssetUrlHelper
+
 def index
 	@clubs = Club.where(:live => true)
 	@hash = Gmaps4rails.build_markers(@clubs) do |club, marker|
 	  marker.lat club.latitude
 	  marker.lng club.longitude
 	  marker.infowindow "<a href='#{club_path(club.slug)}'>#{club.name} Golf Club</a>"
-	  marker.title   "i'm the title"
 	  # marker.picture({
-   #                :url    => "#{club.logo.url(:thumb)}",
+   #                :url    => asset_url('assets/golf-clwb-logo.png'),
    #                :width  => "60",
    #                :height => "60"
    #               })
